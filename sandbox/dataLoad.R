e2db6d028fd339c68eca24a1ffe7e87c20b5a1e5
rossb34 <rossb34@uw.edu>
2013-10-28 12:51:48 -0700

    Initial files for package

---
+++ sandbox/dataLoad.R
@@ -0,0 +1,13 @@
+library(quantMod)
+
+BetaValues2013 <- read.csv("/Users/rossbennett/devel/R/CQA/rawData/BetaValues2013.csv", header=TRUE, as.is=TRUE)
+save(BetaValues2013, file="data/BetaValues2013.Rda")
+# load("data/BetaValues2013.Rda")
+
+mergedData <- read.csv("/Users/rossbennett/devel/R/CQA/rawData/mergedData.txt", header=TRUE,sep="\t",as.is=TRUE,fill=TRUE,quote="")
+save(mergedData, file="data/mergedData.Rda")
+# load("data/mergedData.Rda")
+
+cqaMatchedData <- read.csv("/Users/rossbennett/devel/R/CQA/rawData/cqaMatchedData.txt", header=TRUE,sep="\t",as.is=TRUE,fill=TRUE,quote="")
+save(cqaMatchedData, file="data/cqaMatchedData.Rda")
+# load("data/cqaMatchedData.Rda")
---
+++ sandbox/symbolsDownload.R
@@ -0,0 +1,33 @@
+library(quantmod)
+
+# load the data for the beta values
+data(BetaValues2013)
+
+# This is a character vector of the ticker symbols from the BetaValues2013 file
+tickers <- BetaValues2013[, "Security.Symbol"]
+
+# This will get data from as far back as yahoo has
+from <- "1900-01-01"
+to <- Sys.Date()
+
+# Set the directory to write the data to
+dir <- "~/Documents/tmp/data/"
+
+nTickers <- length(tickers)
+
+# Loop through tickers and load each symbol
+for(i in 1:nTickers){
+  x <- try(getSymbols(Symbols=tickers[i], from=from, to=to, env=NULL), silent=TRUE)
+  if(inherits(x, "try-error")){
+    message(paste("Could not download", tickers[i]))
+  } else {
+    cat("Successfully downloaded", tickers[i], "\n")
+    cat(i, "out of", nTickers, "\n")
+    write.zoo(x, file=paste(dir, tickers[i], ".csv", sep=""), sep=",", col.names=TRUE)
+    rm(x)
+  }
+  # Pause for 5 seconds
+  Sys.sleep(5)
+}
+
+
